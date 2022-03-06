62c2631f221b8b0a1f41d43feb6f46f52f27dbf1
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-06 18:11:25 +0100

    created

---
+++ config/test.exs
@@ -0,0 +1,18 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chat, ChatWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "tY0Cgy54O05R8V5f0lmxTptGzWQ2LoZUNCQHNJO01KvcY2H+KjsZlHFjEwXW8hSr",
+  server: false
+
+# In test we don't send emails.
+config :chat, Chat.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
